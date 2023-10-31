7ace0c17c474416a1f681be0e5a96b3ff1647572
HungVu0112 <pcsolaggy123@gmail.com>
2023-10-31 13:41:00 +0700

    Severside Rendering

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters
+
     const headers = {
 		'X-RapidAPI-Key': 'ee62a308e0mshe97f1d00bb447f3p158496jsn9db572a7fcae',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     })
 
