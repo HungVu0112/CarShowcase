8e3c1837be130a9654e55cc0bcd6084319a5a1af
HungVu0112 <pcsolaggy123@gmail.com>
2023-11-01 23:40:54 +0700

    Clientside Rendering

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,7 +6,7 @@ import { SearchManufacturerProps } from '@/types'
 import { manufacturers } from '@/constants'
 import Image from 'next/image'
 
-const SearchManufacturer = ({ manufacturer, setManufacturer } : SearchManufacturerProps) => {
+const SearchManufacturer = ({ selected, setSelected } : SearchManufacturerProps) => {
   const [query, setQuery] = useState('')
   const filteredManufacturers = 
     query === "" 
@@ -19,7 +19,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer } : SearchManufactur
 
   return (
     <div className='search-manufacturer'>
-      <Combobox value={manufacturer} onChange={setManufacturer}>
+      <Combobox value={selected} onChange={setSelected}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image 
